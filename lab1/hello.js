f993b5aeec01d5a8119d7bd2160be95bc5045cd0
abhaychauhan-lang <abhaychauhan6111@gmail.com>
2026-07-23 11:53:53 +0530

    initial commit

---
+++ lab1/hello.js
@@ -0,0 +1,10 @@
+
+//write a function to take two number and return their sum
+function sum(a,b){ //old method
+    return a+b;
+}
+const add=(a,b)=>{ //arrow function
+    return a+b;
+}
+const add2=(a,b)=>a+b; //advance arrow function
+console.log(sum(9,6));
